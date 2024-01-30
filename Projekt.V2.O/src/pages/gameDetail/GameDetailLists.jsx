ffe5167f6c549c11f79a2fba4820a553e894c456
Mateusz Karnicki <matkarnicki@gmail.com>
2024-01-30 18:14:27 +0100

    dodano funkcjonalnosc znikania pustych list w game details, zmieniono 404.png

--- Projekt.V2.O/src/pages/gameDetail/GameDetailLists.jsx
+++ Projekt.V2.O/src/pages/gameDetail/GameDetailLists.jsx
@@ -4,20 +4,22 @@ import ModifyGameButton from "../buttons/ModifyGameButton";
 export default function GameDetailLists({ gameDetail }) {
   return (
     <div style={{ display: "flex" }}>
-      <ul>
-        <h2>Platforms</h2>
-        {gameDetail?.platforms.map((platform) => (
-          <li key={platform.platform.name}>{platform.platform.name}</li>
-        ))}
-      </ul>
-
-      <ul style={{ marginRight: "20px" }}>
-        <h2>Genres</h2>
-        {gameDetail?.genres.map((genre) => (
-          <li key={genre.name}>{genre.name}</li>
-        ))}
-      </ul>
-
+      {gameDetail?.platforms?.length > 0 && (
+        <ul>
+          <h2>Platforms</h2>
+          {gameDetail?.platforms.map((platform) => (
+            <li key={platform.platform.name}>{platform.platform.name}</li>
+          ))}
+        </ul>
+      )}
+      {gameDetail?.genres?.length > 0 && (
+        <ul style={{ marginRight: "20px" }}>
+          <h2>Genres</h2>
+          {gameDetail?.genres.map((genre) => (
+            <li key={genre.name}>{genre.name}</li>
+          ))}
+        </ul>
+      )}
       <ul>
         {gameDetail?.my_developers?.length > 0 && (
           <div>
